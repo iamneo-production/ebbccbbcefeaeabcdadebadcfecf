f9527471f64a8a1bde8462bdf162cdd89da36f96
Iamneo <techadmin@iamneo.ai>
2023-08-01 10:39:28 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,21 +7,4 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent {
   title = 'angularapp';
-
-  userForm!: FormGroup;
-  ngOnInit()
-  {
-    this.userForm=new FormGroup({
-      'inputFirstname':new FormControl(null,[Validators.required,Validators.maxLength(50),Validators.minLength(3),Validators.pattern('[a-z]*')]),
-      'gender':new FormControl(null,Validators.required),
-      'country':new FormControl(null,Validators.required),
-      'city':new FormControl(null,Validators.required),
-      'inputAge':new FormControl(null)
-    });
-  }
-  checkAgeValid(country:string,age:string){
-    if((country=='US' || country=='CA' || country=='IN' || country=='') && (age=='' || isNaN(parseInt(age))))
-    return true;
-    return false;
-  }
 }
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,9 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
